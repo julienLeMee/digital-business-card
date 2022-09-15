db09eb296f3f881f77159cae5ecd27c15e072127
Julien Le Mee <julemee@gmail.com>
2022-09-15 00:15:27 -0400

    update some style

--- components/Info.jsx
+++ components/Info.jsx
@@ -5,7 +5,7 @@ export default function Info() {
     <div id='info' className="info">
       <h1 className='name'>Julien Le Mée</h1>
       <h2 className='function'>Développeur web Full-Stack</h2>
-      <h4 className="portfolio"><a href="http://www.julienlemee.com">Portfolio</a></h4>
+      <h4 className="portfolio"><a href="http://www.julienlemee.com">www.julienlemee.com</a></h4>
       <button className='button-mail'><i class="fa-regular fa-envelope"></i>E-mail</button>
       <button className='button-linkedin'><i class="fab fa-linkedin"></i>Linkedin</button>
     </div>
